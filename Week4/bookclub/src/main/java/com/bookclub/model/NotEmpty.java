ebfddc74dc8f1bc88009c79cce907045035a43ab
ejylha <143954673+ejylha@users.noreply.github.com>
2025-04-06 21:39:53 -0400

    Add files via upload

---
+++ Week4/bookclub/src/main/java/com/bookclub/model/NotEmpty.java
@@ -0,0 +1,5 @@
+package com.bookclub.model;
+
+public @interface NotEmpty {
+
+}
---
+++ Week4/bookclub/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // This tells Spring Boot to look for index.html in /templates/
+    }
+}
